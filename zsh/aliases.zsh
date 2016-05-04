eca38466bdad6df662d44c9b72562d50ddbebdb9
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-05-04 16:21:06 +0300

    Add couple of git, one for hosts and some deploy aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,11 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias cpd='cap production deploy'
+alias cpdsc='cap production deploy:sitemap:create'
+alias cpdsr='cap production deploy:sitemap:refresh'
+alias cps='cap production setup'
+alias csd='cap staging deploy'
+
+alias hosts='sudo subl /etc/hosts'
